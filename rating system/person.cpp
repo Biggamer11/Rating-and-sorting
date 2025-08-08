03420deade5caa77b9b236908065198f8b2b6785
Raven <jetace64@gmail.com>
2025-08-08 10:24:09 -0400

    Rename vote map and clarify documentation

--- rating system/person.cpp
+++ rating system/person.cpp
@@ -12,7 +12,8 @@ void person::setStratum()
     
 }
 
-/**/
+// Constructor initializes a person with an ID and starting rating.
+// The votes map collects ratings keyed by voter ID.
 person::person(int id, double number)
 {
     pid = id;
